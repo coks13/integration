9ab672919a3bea764abd72611f03f2af823a0822
corentin <corentin.surmaire@gmail.com>
2022-02-18 10:14:29 +0100

    ajout

--- java.js
+++ java.js
@@ -1,11 +1,11 @@
-let listeCourse = document.getElementById("listeCourse");
+let listePourCourse = document.getElementById("listePourCourse");
 let planning = document.getElementById("planning");
 let ingredients = document.getElementById("ingredients");
 let rubrique = document.getElementById("rubrique");
 
 ingredients.style.display = "none";
 
-listeCourse.addEventListener("click", () => {
+listePourCourse.addEventListener("click", () => {
   if(getComputedStyle(rubrique).display != "none"){
     rubrique.style.display = "none";
     ingredients.style.display = "block";
